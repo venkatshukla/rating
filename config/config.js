677b8c6a8961362bb7c8748c51d1780be0b2c59b
I334477 <venkat.shukla@sap.com>
2019-06-15 13:25:51 +0530

    project boilerplate

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "development": {
+        "username": "bu9jbj3rlu5c1p2m",
+        "password": "p3de3wcx68c9ju6p",
+        "database": "ratings_test",
+        "host": "u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306",
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "bu9jbj3rlu5c1p2m",
+        "password": "p3de3wcx68c9ju6p",
+        "database": "ratings_test",
+        "host": "u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": "bu9jbj3rlu5c1p2m",
+        "password": "p3de3wcx68c9ju6p",
+        "database": "ratings_test",
+        "host": "u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306",
+        "dialect": "mysql"
+    }
+  }
