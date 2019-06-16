10558b10d60961226b5721a2d17d170e4eec1d4c
I334477 <venkat.shukla@sap.com>
2019-06-17 01:32:18 +0530

    added production config

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     "production": {
         "username": "bu9jbj3rlu5c1p2m",
         "password": "p3de3wcx68c9ju6p",
-        "database": "ratings_test",
+        "database": "ubota2tf1hn2sh2p",
         "host": "u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
         "port": 3306,
         "dialect": "mysql"
